d01e009278266d276c7035a2c8f62a3a6b0e8b3f
John Doe <johndoe@example.com>
2022-07-27 19:57:16 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text" v-model="text">
+  <button @click="setText">Sent</button>
+  <p v-for="text in textArray" :key="text.message">
+  {{text.message}}
+  </p>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+      text:"",
+      textArray:[]
+      
+    }
+  },
+  methods:{
+    setText(){
+      this.textArray.push({message: this.text})
+    }
+  },
+  props:{
+    // textArray
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ClientFirst.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="text" v-model="text">
+  <button @click="setText">Sent</button>
+    <ul v-for="item in textArray" :key="item.message"></ul>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+      text:"",
+      textArray:[]
+      
+    }
+  },
+  methods:{
+    setText(){
+      this.textArray.push({message: this.text})
+    }
+  },
+  props:{
+    textArray
+  }
+}
+</script>
+
+<style>
+
+</style>
